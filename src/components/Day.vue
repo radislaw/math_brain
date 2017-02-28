85cabbc028c076851dd83242b16638fb2a2af554
llv <llv@feip.co>
2017-02-28 18:32:52 +1000

    change validation

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -11,28 +11,29 @@
 
 <template>
     <div>
-        <h1>{{ title }}</h1>
+        <h1>{{ title }} {{ day + 1 }}</h1>
         <!--<a href="/result">Result</a>-->
         <h2>{{currentQuestion}}</h2>
         <form @submit.prevent="changeQuestion">
             <input type="number" class="answer_input" v-model="inputValue">
         </form>
-        <p>Количество верных ответов: {{totalRightAnswers}} из {{questionNumber}}</p>
+        <p>Количество верных ответов: {{totalRightAnswers}} из {{totalAnswers}}</p>
         <p>Количество неверных ответов: {{totalWrongAnswers}}</p>
         <p>Время потраченное на решение примеров: {{timeSpend}}</p>
     </div>
 </template>
 
 <script>
-  import * as task from '../tasks.json';
+  //  import * as task from '../tasks.json';
+  import task from '../tasks';
 
   export default {
     name: 'day',
     data() {
       return {
-        title: 'Первый день',
-        day: 1,
-        questionNumber: 1,
+        title: 'День',
+        day: 0,
+        questionNumber: 0,
         task,
         timeSpend: 0,
         inputValue: '',
@@ -42,16 +43,23 @@
     },
     computed: {
       currentQuestion() {
-        return this.task[this.day][this.questionNumber].question;
+        return this.task[this.day][this.questionNumber];
+      },
+      totalAnswers(){
+        return this.task[this.day].length;
       },
       totalRightAnswers() {
-        if (+this.inputValue === +this.task[this.day][this.questionNumber].answer) {
+        if (+this.inputValue === eval(this.task[this.day][this.questionNumber])
+            && this.questionNumber < this.task[this.day].length) {
           this.rightAnswers += 1;
         }
         return this.rightAnswers;
       },
       totalWrongAnswers() {
-        if (+this.inputValue !== +this.task[this.day][this.questionNumber].answer && this.inputValue !== '' && this.questionNumber < 8) {
+        if (+this.inputValue !== eval(this.task[this.day][this.questionNumber])
+            && this.inputValue !== ''
+            && this.questionNumber < this.task[this.day].length) {
+          console.log('количество вопросов ' + this.task[this.day].length);
           this.wrongAnswers += 1;
         }
         return this.wrongAnswers;
@@ -59,10 +67,12 @@
     },
     methods: {
       changeQuestion() {
-        if (this.questionNumber <= 7) {
+        if (this.questionNumber <= this.task[this.day].length) {
           this.questionNumber += 1;
           this.inputValue = '';
 //          console.log(this.task[this.day][this.questionNumber - 1].answer);
+//        } else if (this.questionNumber > 7) {
+//          this.day += 1;
         }
       },
     },
